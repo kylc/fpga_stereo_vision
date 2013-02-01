33336b42974bf4671788e9fe4c66cac900ef9f80
kylc <kcesare@gmail.com>
2013-01-31 23:08:15 -0800

    Add a more complete 'prefilter' testbench.
    This implementation loads a test images, applies the filter, and saves a copy
    for analysis.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 compile_stereobm_prefilter_tb:
-	gcc -std=c99 -Iinclude/ -Wall -o stereobm_prefilter_tb src/*.c test/stereobm_prefilter_tb.c
+	gcc -std=c99 -Iinclude/ -I/usr/include/ -Wall -o stereobm_prefilter_tb src/*.c test/stereobm_prefilter_tb.c -lnetpbm
--- test/stereobm_prefilter_tb.c
+++ test/stereobm_prefilter_tb.c
@@ -1,25 +1,43 @@
 #include <stdlib.h>
-#include <time.h>
+#include <pam.h>
 
 #include "stereobm_image.h"
 #include "stereobm_prefilter.h"
 
-int main() {
-	// Seed the random number generator
-	srand(time(NULL));
+int main(int argc, char **argv) {
+	// Open the test test image
+	pm_init(argv[0], 0);
 
-	// Generate a test image
+	int cols, rows;
+	pixval max;
+	FILE *fp = fopen("test_img.ppm", "r");
+	pixel **data = ppm_readppm(fp, &cols, &rows, &max);
+
+	// Copy data into internal data structure
 	struct rgb_image_t image;
 	for(int x = 0; x < IMG_WIDTH; x++) {
 		for(int y = 0; y < IMG_HEIGHT; y++) {
-		    image.pixels[x][y].r = rand() % 255;
-		    image.pixels[x][y].g = rand() % 255;
-		    image.pixels[x][y].b = rand() % 255;
+			// PPM data is indexed by y, then x
+			image.pixels[x][y].r = PPM_GETR(data[y][x]);
+			image.pixels[x][y].g = PPM_GETG(data[y][x]);
+			image.pixels[x][y].b = PPM_GETB(data[y][x]);
 		}
 	}
 
 	// Apply the prefilter
 	stereobm_prefilter(&image);
 
-	return 0;
+	// Copy data back into PPM structure
+	for(int x = 0; x < IMG_WIDTH; x++) {
+		for(int y = 0; y < IMG_HEIGHT; y++) {
+			// PPM data is indexed by y, then x
+			struct rgb8_t rgb = image.pixels[x][y];
+			PPM_ASSIGN(data[y][x], rgb.r, rgb.g, rgb.b);
+		}
+	}
+
+	FILE *outfp = fopen("test_img_out.ppm", "w");
+	ppm_writeppm(outfp, data, cols, rows, max, 0);
+
+	return EXIT_SUCCESS;
 }
