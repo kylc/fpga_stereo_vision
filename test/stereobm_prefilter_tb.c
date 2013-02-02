345287055e6b6ec273c6f25319408382240f32fd
kylc <kcesare@gmail.com>
2013-02-01 22:18:57 -0800

    Extract testbench image loading/saving to a 'tb_util' file.

--- test/stereobm_prefilter_tb.c
+++ test/stereobm_prefilter_tb.c
@@ -1,44 +1,16 @@
-#include <stdlib.h>
-#include <pam.h>
-
 #include "stereobm_image.h"
 #include "stereobm_prefilter.h"
+#include "tb_util.c"
 
 int main(int argc, char **argv) {
 	// Open the test test image
-	pm_init(argv[0], 0);
-
-	int cols, rows;
-	pixval max;
-	FILE *fp = fopen("test_img.ppm", "r");
-	pixel **data = ppm_readppm(fp, &cols, &rows, &max);
-
-	// Copy data into internal data structure
-	struct rgb_image_t image;
-	for(int x = 0; x < IMG_WIDTH; x++) {
-		for(int y = 0; y < IMG_HEIGHT; y++) {
-			// PPM data is indexed by y, then x
-			image.pixels[x][y].r = PPM_GETR(data[y][x]);
-			image.pixels[x][y].g = PPM_GETG(data[y][x]);
-			image.pixels[x][y].b = PPM_GETB(data[y][x]);
-		}
-	}
+	struct rgb_image_t image = load_rgb_ppm("test_images/test_img.ppm");
 
 	// Apply the prefilter
 	struct g_image_t prefiltered = stereobm_prefilter(&image);
 
 	// Copy data back into PPM structure
-	for(int x = 0; x < IMG_WIDTH; x++) {
-		for(int y = 0; y < IMG_HEIGHT; y++) {
-                        struct g8_t g = prefiltered.pixels[x][y];
-
-			// PPM data is indexed by y, then x
-			PPM_ASSIGN(data[y][x], g.g, g.g, g.g);
-		}
-	}
-
-	FILE *outfp = fopen("test_img_out.ppm", "w");
-	ppm_writeppm(outfp, data, cols, rows, max, 0);
+        save_grayscale_ppm("test_images/test_img_out.ppm", &prefiltered);
 
 	return EXIT_SUCCESS;
 }
---
+++ test/tb_util.c
@@ -0,0 +1,61 @@
+#include <pam.h>
+
+#include "stereobm_image.h"
+
+struct rgb_image_t load_rgb_ppm(char *file_name);
+void save_grayscale_ppm(char *file_name, struct g_image_t *image);
+void save_depth_ppm(char *file_name, struct depth_image_t *image);
+
+struct rgb_image_t load_rgb_ppm(char *file_name) {
+	int cols, rows;
+	pixval max;
+	FILE *fp = fopen(file_name, "r");
+	pixel **data = ppm_readppm(fp, &cols, &rows, &max);
+
+	// Copy data into internal data structure
+	struct rgb_image_t image;
+	for(int x = 0; x < IMG_WIDTH; x++) {
+		for(int y = 0; y < IMG_HEIGHT; y++) {
+			// PPM data is indexed by y, then x
+			image.pixels[x][y].r = PPM_GETR(data[y][x]);
+			image.pixels[x][y].g = PPM_GETG(data[y][x]);
+			image.pixels[x][y].b = PPM_GETB(data[y][x]);
+		}
+	}
+
+	return image;
+}
+
+void save_grayscale_ppm(char *file_name, struct g_image_t *image) {
+	pixel **data = ppm_allocarray(IMG_WIDTH, IMG_HEIGHT);
+
+	// Copy data back into PPM structure
+	for(int x = 0; x < IMG_WIDTH; x++) {
+		for(int y = 0; y < IMG_HEIGHT; y++) {
+                        struct g8_t pixel = image->pixels[x][y];
+
+			// PPM data is indexed by y, then x
+			PPM_ASSIGN(data[y][x], pixel.g, pixel.g, pixel.g);
+		}
+	}
+
+	FILE *outfp = fopen(file_name, "w");
+	ppm_writeppm(outfp, data, IMG_WIDTH, IMG_HEIGHT, PPM_MAXMAXVAL, 0);
+}
+
+void save_depth_ppm(char *file_name, struct depth_image_t *image) {
+	pixel **data = ppm_allocarray(IMG_WIDTH, IMG_HEIGHT);
+
+	// Copy data back into PPM structure
+	for(int x = 0; x < IMG_WIDTH; x++) {
+		for(int y = 0; y < IMG_HEIGHT; y++) {
+                        struct depth8_t p = image->pixels[x][y];
+
+			// PPM data is indexed by y, then x
+			PPM_ASSIGN(data[y][x], p.d, p.d, p.d);
+		}
+	}
+
+	FILE *outfp = fopen(file_name, "w");
+	ppm_writeppm(outfp, data, IMG_WIDTH, IMG_HEIGHT, PPM_MAXMAXVAL, 0);
+}
