f56c5f5f7a9a7fc46bff289b29ff02246c628f65
kylc <kcesare@gmail.com>
2013-02-01 22:26:16 -0800

    Add a stereobm_core testbench.

--- test/stereobm_core_tb.c
+++ test/stereobm_core_tb.c
@@ -1,12 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "stereobm_core.h"
+#include "stereobm_image.h"
+#include "tb_util.c"
 
-int main() {
-	double out = 5;
+int main(int argc, char **argv) {
+	// Open the test  images
+	struct rgb_image_t left_image = load_rgb_ppm("test_images/bowling2_left.ppm");
+	struct rgb_image_t right_image = load_rgb_ppm("test_images/bowling2_right.ppm");
 
-	printf("Output: %f\n", out);
+	// Run the core
+	struct depth_image_t depth = stereobm_core(&left_image, &right_image);
 
-	return 0;
+	// Copy data back into PPM structure
+        save_depth_ppm("test_images/bowling2_out.ppm", &depth);
+
+	return EXIT_SUCCESS;
 }
