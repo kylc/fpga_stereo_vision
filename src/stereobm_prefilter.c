c91bbc5eefb9dbe0b5b8cebf6f04216fd833bc56
Kyle Cesare <kcesare@gmail.com>
2013-02-05 17:02:14 -0800

    Initial pass of 'stereobm_sobel'.  Still need to verify.

--- src/stereobm_prefilter.c
+++ src/stereobm_prefilter.c
@@ -7,7 +7,7 @@
 struct g_image_t stereobm_prefilter(struct rgb_image_t *image) {
 	struct g_image_t grayscale = stereobm_convert_to_grayscale(image);
 
-	grayscale = stereobm_sobel(&grayscale);
+	// grayscale = stereobm_sobel(&grayscale);
 
 	return grayscale;
 }
@@ -30,15 +30,37 @@ struct g_image_t stereobm_convert_to_grayscale(struct rgb_image_t *image) {
 }
 
 /* Applies a Sobel filter to the image */
-struct g_image_t stereobm_sobel(struct g_image_t *image) {
-	struct g_image_t sobel;
+void stereobm_sobel(struct g_image_t *image, struct g_image_t *output) {
+	// We will create a 3x3 window centered at P = (x, y) and shift it to the
+	// left at each iteration.
+	unsigned char buf[3][3];
+#pragma HLS ARRAY_PARTITION variable=buf complete dim=0
 
 	for(int x = 1; x < IMG_WIDTH - 1; x++) {
 		for(int y = 1; y < IMG_HEIGHT - 1; y++) {
+#pragma HLS PIPELINE
+
+			// P = (x, y)
+			// Shift out the left 3rd of the window
+			for(int i = 0; i < 3; i++) {
+#pragma HLS UNROLL
+				buf[0][i] = buf[1][i];
+				buf[1][i] = buf[2][i];
+			}
+
+			// Shift in new right third
+			for(int i = 0; i < 3; i++) {
+#pragma HLS UNROLL
+				// TODO: This buffer shift required 3 memory reads, but RAM
+				// only has two ports.  This make the pipeline II=2, doubling
+				// the execution cycles.
+				buf[2][i] = image->pixels[x + 1][y + i - 1].g;
+			}
+
 			// Compute the matrix product.
-			int d0 = image->pixels[x + 1][y - 1].g - image->pixels[x - 1][y - 1].g;
-			int d1 = image->pixels[x + 1][y].g - image->pixels[x - 1][y].g;
-			int d2 = image->pixels[x + 1][y + 1].g - image->pixels[x - 1][y + 1].g;
+			int d0 = buf[2][0] - buf[0][0];
+			int d1 = buf[2][1] - buf[0][1];
+			int d2 = buf[2][2] - buf[0][2];
 
 			int value = d0 + (2 * d1) + d2 + (SOBEL_CLAMP / 2);
 
@@ -49,9 +71,7 @@ struct g_image_t stereobm_sobel(struct g_image_t *image) {
 				value = SOBEL_CLAMP;
 			}
 
-			sobel.pixels[x][y].g = value;
+			output->pixels[x][y].g = value;
 		}
 	}
-
-	return sobel;
 }
