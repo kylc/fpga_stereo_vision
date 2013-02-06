683bb8dd03f2ac59c9c478fbe8120bda270ed595
Kyle Cesare <kcesare@gmail.com>
2013-02-05 17:02:14 -0800

    Fully optimized 'stereobm_sobel'.

--- src/stereobm_prefilter.c
+++ src/stereobm_prefilter.c
@@ -6,10 +6,12 @@
 
 struct g_image_t stereobm_prefilter(struct rgb_image_t *image) {
 	struct g_image_t grayscale = stereobm_convert_to_grayscale(image);
+	struct g_image_t sobel;
 
-	grayscale = stereobm_sobel(&grayscale);
+	// grayscale = stereobm_sobel(&grayscale);
+	stereobm_sobel(&grayscale, &sobel);
 
-	return grayscale;
+	return sobel;
 }
 
 /* Convert an RGB image to grayscale using the RGB average */
@@ -30,15 +32,32 @@ struct g_image_t stereobm_convert_to_grayscale(struct rgb_image_t *image) {
 }
 
 /* Applies a Sobel filter to the image */
-struct g_image_t stereobm_sobel(struct g_image_t *image) {
-	struct g_image_t sobel;
+void stereobm_sobel(struct g_image_t *image, struct g_image_t *output) {
+	// We will create a 3x3 window centered at P = (x, y) and shift it to the
+	// left at each iteration.
+	unsigned char buf[3][3];
+#pragma HLS ARRAY_PARTITION variable=buf complete dim=0
 
 	for(int x = 1; x < IMG_WIDTH - 1; x++) {
 		for(int y = 1; y < IMG_HEIGHT - 1; y++) {
+#pragma HLS PIPELINE
+			// Shift each row up
+			for(int i = 0; i < 3; i++) {
+#pragma HLS UNROLL
+				buf[i][0] = buf[i][1];
+				buf[i][1] = buf[i][2];
+			}
+
+			// Shift in new bottom row.  We don't need the center
+			// row.  Omitting it allows us to fit our reads into a
+			// 2P RAM every cycle.
+			buf[0][2] = image->pixels[x - 1][y].g;
+			buf[2][2] = image->pixels[x + 1][y].g;
+
 			// Compute the matrix product.
-			int d0 = image->pixels[x + 1][y - 1].g - image->pixels[x - 1][y - 1].g;
-			int d1 = image->pixels[x + 1][y].g - image->pixels[x - 1][y].g;
-			int d2 = image->pixels[x + 1][y + 1].g - image->pixels[x - 1][y + 1].g;
+			int d0 = buf[2][0] - buf[0][0];
+			int d1 = buf[2][1] - buf[0][1];
+			int d2 = buf[2][2] - buf[0][2];
 
 			int value = d0 + (2 * d1) + d2 + (SOBEL_CLAMP / 2);
 
@@ -49,9 +68,7 @@ struct g_image_t stereobm_sobel(struct g_image_t *image) {
 				value = SOBEL_CLAMP;
 			}
 
-			sobel.pixels[x][y].g = value;
+			output->pixels[x][y].g = value;
 		}
 	}
-
-	return sobel;
 }
