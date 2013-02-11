e9cdcaff672dd06aa657a67eb404c86702050498
kylc <kcesare@gmail.com>
2013-02-10 16:40:14 -0800

    Only use progressive SAD score for now.
    This messes up the left 'SAD_WINDOW_SIZE' columns of the disparity map, but
    makes the optimization much easier for the HLS compiler.  Will need to find a
    way to fill the buffer to fix the output, however.

--- src/stereobm_correspondence.c
+++ src/stereobm_correspondence.c
@@ -36,9 +36,10 @@ struct depth_image_t stereobm_correspondence(struct g_image_t *left_image, struc
 	for(uint8_t d = MIN_DISPARITY; d < MAX_DISPARITY; d++) {
 		for(uint16_t y = 0; y < IMG_HEIGHT; y++) {
 			for(uint16_t x = MAX_DISPARITY; x < IMG_WIDTH; x++) {
-				int32_t score = x == MAX_DISPARITY ?
-					stereobm_sad_score(left_image, right_image, x, y, d, score_buffer)
-					: stereobm_progressive_sad_score(left_image, right_image, x, y, d, score_buffer, prev_score);
+				// int32_t score = x == MAX_DISPARITY ?
+				// 	stereobm_sad_score(left_image, right_image, x, y, d, score_buffer)
+				//	: stereobm_progressive_sad_score(left_image, right_image, x, y, d, score_buffer, prev_score);
+				int32_t score = stereobm_progressive_sad_score(left_image, right_image, x, y, d, score_buffer, prev_score);
 
 				// If this 'd' value is better than the current
 				// best, record it.
