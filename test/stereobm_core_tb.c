268d8460ff283ee241d18852ee177d03ade47a26
Kyle Cesare <kcesare@gmail.com>
2013-04-11 00:40:27 -0700

    Clean up test images.

--- test/stereobm_core_tb.c
+++ test/stereobm_core_tb.c
@@ -4,14 +4,14 @@
 
 int main(int argc, char **argv) {
 	// Open the test  images
-	struct rgb_image_t left_image = load_rgb_ppm("test_images/im2.ppm");
-	struct rgb_image_t right_image = load_rgb_ppm("test_images/im6.ppm");
+	struct rgb_image_t left_image = load_rgb_ppm("test_images/cones_left.ppm");
+	struct rgb_image_t right_image = load_rgb_ppm("test_images/cones_right.ppm");
 
 	// Run the core
 	struct depth_image_t depth = stereobm_core(&left_image, &right_image);
 
 	// Copy data back into PPM structure
-	save_depth_ppm("test_images/im26_out.ppm", &depth, DISPARITY_CLAMP);
+	save_depth_ppm("test_images/cones_out.ppm", &depth, DISPARITY_CLAMP);
 
 	return EXIT_SUCCESS;
 }
--- test/stereobm_prefilter_tb.c
+++ test/stereobm_prefilter_tb.c
@@ -4,13 +4,13 @@
 
 int main(int argc, char **argv) {
 	// Open the test test image
-	struct rgb_image_t image = load_rgb_ppm("test_images/im2.ppm");
+	struct rgb_image_t image = load_rgb_ppm("test_images/cones_left.ppm");
 
 	// Apply the prefilter
 	struct g_image_t prefiltered = stereobm_prefilter(&image);
 
 	// Copy data back into PPM structure
-	save_grayscale_ppm("test_images/im2_out.ppm", &prefiltered, SOBEL_CLAMP);
+	save_grayscale_ppm("test_images/cones_prefiltered.ppm", &prefiltered, SOBEL_CLAMP);
 
 	return EXIT_SUCCESS;
 }
